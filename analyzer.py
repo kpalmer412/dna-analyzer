71800fb271eef0a30d7684f3a38f474228704986
Ken Palmer <kenpalmer412@gmail.com>
2026-07-06 16:13:24 -0400

    Add RNA transcription feature to bioinformatics engine

--- analyzer.py
+++ analyzer.py
@@ -2,3 +2,6 @@ def calculate_gc_content(sequence):
     bases = sequence.upper()
     gc_count = bases.count('G') + bases.count('C')
     return (gc_count / len(bases)) * 100 if bases else 0
+
+def transcribe_to_rna(sequence):
+    return sequence.upper().replace('T', 'U')
